37f734383d255f9f520bc69a29cf313ff6662d83
Alec Livaditis <aleclivaditis@gmail.com>
2023-07-21 18:25:06 -0400

    add searchAlbums API call

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -4,9 +4,34 @@ import './Search.css'
 function Search({setResults}) {
   const [inputValue, setInput] = useState('')
 
+  function searchAlbums(query) {
+    return fetch(`https://api.discogs.com/database/search?q=${query}&type=master&key=mbubAaAXseWPUpaJLkKU&secret=TrELhUezCNdFoIfmoAdHZmfJIXljOSfW&format=vinyl`)
+      .then(res => {
+        if (!res.ok) {
+        throw new Error('Trailer not found.')
+        }
+        return res.json()})
+      .then(data => data.results)
+      .then(results => {
+        return results.map(result => {
+          const splitTitle = result.title.split(' - ')
+          const [artist, title] = splitTitle
+          return {
+            masterId: result.master_id,
+            title,
+            artist,
+            thumb: result.thumb,
+            coverImage: result.cover_image
+          }
+        })
+      })
+  }
+  
   function handleClick(event) {
     event.preventDefault()
-    setResults('Yea')
+    searchAlbums(inputValue)
+      .then(results => setResults(results))
+      .catch(error => alert(error.message))
     setInput('')
   }
 
