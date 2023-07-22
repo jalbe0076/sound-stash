b20aa7d53e18784a97ad7c7ed17e6c5eed0914ff
Joshua Martin <ursa8minority@gmail.com>
2023-07-22 12:35:01 -0600

    feat/username_login/password with error alert handle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Login from './Login';
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleLogin = (username, password) => {
+    if (username === 'user1' && password === 'password') {
+      setLoggedInUser(username);
+    } else {
+      alert('Invalid username or password');
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+      {loggedInUser ? (
+          <h1>Welcome, {loggedInUser}!</h1>
+        ) : (
+          <Login onLogin={handleLogin} />
+        )}
       </header>
     </div>
   );
--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
-import backgroundImage from './Assets/Band2.png';
 import logo from './Assets/logo.png';
 
-const Login = () => {
-  const loginContainerStyles = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(username, password);
   };
 
   return (
-    <div className="login-container" style={loginContainerStyles}>
+    <div className="login-container">
       <div className="logo-container">
         <img src={logo} alt="Logo" className="logo" />
       </div>
       <div className="navbar">
-        <input type="text" placeholder="Username" className="username-field" />
-        <input type="password" placeholder="Password" className="password-field" />
-        <button className="login-button">Login</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            className="username-field"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="password-field"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="login-button">
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
