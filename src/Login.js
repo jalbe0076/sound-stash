f52b16b663deda97b37f78b94eb7048a6edd2473
Joshua Martin <ursa8minority@gmail.com>
2023-07-22 11:43:41 -0600

    CSS refactor for the logo insert above inputs

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Login.css';
+import logo from './Assets/logo.png';
 
 const Login = () => {
   return (
     <div className="login-container">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
       <div className="navbar">
         <input type="text" placeholder="Username" className="username-field" />
         <input type="password" placeholder="Password" className="password-field" />
