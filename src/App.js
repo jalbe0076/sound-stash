01d0a7396740ef2ed5d599c7548dafa3ceff49bd
Jason Alberto <alberto.jason@gmail.com>
2023-07-23 11:17:01 -0400

    Create navigation routes in App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav/Nav';
-
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Routes>
+        <Route path="/" />
+        <Route path="/journal" /*element={<Journal />}*/ />
+        <Route path="/collections" /*element={<Collections />}*/ />
+        <Route path="/discover" /*element={<Discover />}*/ />
+      </Routes>
     </div>
   );
 }
