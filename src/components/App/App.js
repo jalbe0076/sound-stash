707e1da68cbf980eb75ce098ba3d502f2bb79126
Jason Alberto <alberto.jason@gmail.com>
2023-07-24 22:42:11 -0400

    Copied code from original search PR to eliminate bug with hidden files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,22 +5,27 @@ import Discover from '../Discover/Discover';
 import Collections from '../Collections/Collections';
 import Journal from '../Journal/Journal';
 import EmptyState from '../EmptyState/EmptyState';
+import Search from '../Search/Search'
+import Results from '../Results/Results'
+import Album from '../Album/Album'
 
 function App() {
   return (
     <>
       <Nav />
+      <Search/>
       <main className="App">
         <Routes>
           <Route path="/" />
           <Route path="/journal" element={<Journal />} />
           <Route path="/collections" element={<Collections />} />
           <Route path="/discover" element={<Discover />} />
+          <Route path='/search/:query/:page' element={<Results/>}/>
+          <Route path='/albums/:id' element={<Album/>}/>
           <Route path="*" element={<EmptyState />} />
         </Routes>
       </main>
     </>
   );
 }
-
 export default App;
