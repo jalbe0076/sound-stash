599da6583febf1fa0cefdb6be132475e65d35a2f
Alec Livaditis <aleclivaditis@gmail.com>
2023-07-26 19:03:08 -0400

    add show notes feature

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -4,9 +4,11 @@ import'./Entry.css'
 
 function Entry ({id, title, artists, date, notes, image, rating}) {
   const [notesHidden, setNotesHidden] = useState(true)
+  const [notesIcon, setIcon] = useState('/images/sticky-note-white.png')
   
   const showNotes = () => {
     notesHidden ? setNotesHidden(false) : setNotesHidden(true)
+    notesIcon === '/images/sticky-note-white.png' ? setIcon('/images/sticky-note-pink.png') : setIcon('/images/sticky-note-white.png')
   }
 
   return (
@@ -21,7 +23,7 @@ function Entry ({id, title, artists, date, notes, image, rating}) {
           <p className='entry-artist'>{artists}</p>
           {rating>0 && <Rating initialValue={rating} readonly={true} size='12'/>}
         </div>
-        {!notes ? null : <button onClick={showNotes}>show</button>}
+        {!notes ? null : <img className={`notes-icon`} src={notesIcon} onClick={showNotes}/>}
         <button className='entry-delete'>DELETE</button>
       </div>
       {!notesHidden && <p className='entry-notes'>{notes}</p>}
