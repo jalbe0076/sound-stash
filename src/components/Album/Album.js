3364e840a50b73d7cbb6a2c279cd7e5097233e65
Joshua Martin <ursa8minority@gmail.com>
2023-07-26 16:38:03 -0600

    Album component updated to fetch and render from an API based on the provided id

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -1,10 +1,39 @@
-import React from "react"
-import { useParams } from "react-router-dom"
-import './Album.css'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getAlbumDetails } from '../../api';
+
 function Album() {
   const {id} = useParams()
+  const [albumDetails, setAlbumDetails] = useState(null);
+  
+  useEffect(() => {
+    getAlbumDetails(id)
+      .then(data => setAlbumDetails(data))
+      .catch(error => alert(error.message));
+  }, [id]);
+
+  if (!albumDetails) {
+    return <div>Loading...</div>;
+  }
+  const { title, artist, releaseDate, label, genre, styles, tracklist, coverImg } = albumDetails;
+
   return (
-    <p>{id} hello</p>
-  )
+    <div>
+    <img src={coverImg} alt={`Cover art for ${title}`} />
+    <h2>{title}</h2>
+    <p>Artist: {artist}</p>
+    <p>Release Date: {releaseDate}</p>
+    <p>Label: {label}</p>
+    <p>Genre: {genre}</p>
+    <p>Styles: {styles.join(', ')}</p>  
+    <h3>Tracklist:</h3>
+    <ul>
+      {tracklist.map((track, index) => (
+        <li key={index}>{track}</li>
+      ))}
+    </ul>
+  </div>
+  );
 }
+
 export default Album
