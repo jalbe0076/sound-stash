74982cc6d4ef4e875b19b97bd29ee1a5fb8c8ae7
Alec Livaditis <aleclivaditis@gmail.com>
2023-07-26 12:17:31 -0400

    add erorr handling for broken search result img

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,7 @@ import './Card.css'
 function Card({result}) {
   return (
     <Link to={`/albums/${result.masterId}`} className='results--card' id={result.masterId}>
-      <img className='results--image' src={result.thumb}/>
+      <img className='results--image' src={!result.thumb ? process.env.PUBLIC_URL + "/images/broken-record.png" : result.thumb}/>
       <p className='results--title'>{result.title.length > 20 ? result.title.slice(0, 20) + '...' : result.title}</p>
       <p className='results--artist'>{result.artist.length > 20 ? result.artist.slice(0, 20) + '...' : result.artist}</p>
     </Link>
