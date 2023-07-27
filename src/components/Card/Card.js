ac8815d104b94f4b3d9c5c18054334b388e70359
Alec Livaditis <aleclivaditis@gmail.com>
2023-07-26 20:12:19 -0400

    add onError function for img in results

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom'
 import './Card.css'
 
 function Card({result}) {
+  const onImageError = (e) => {
+    e.target.src = process.env.PUBLIC_URL + "/images/broken-record.png"
+  }
+  
   return (
     <Link to={`/albums/${result.masterId}`} className='results--card' id={result.masterId}>
-      <img className='results--image' src={!result.thumb ? process.env.PUBLIC_URL + "/images/broken-record.png" : result.thumb}/>
+      <img className='results--image' src={result.thumb ? result.thumb : process.env.PUBLIC_URL + "/images/broken-record.png"} onError={onImageError}/>
       <p className='results--title'>{result.title.length > 20 ? result.title.slice(0, 20) + '...' : result.title}</p>
       <p className='results--artist'>{result.artist.length > 20 ? result.artist.slice(0, 20) + '...' : result.artist}</p>
     </Link>
