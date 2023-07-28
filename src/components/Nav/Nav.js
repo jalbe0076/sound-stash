f88f7f736294713fe3ebfebc30e5d1aef5e2f6eb
Jason Alberto <alberto.jason@gmail.com>
2023-07-28 16:56:10 -0400

    Add conditional to hide navbar when there is no user logged in

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,21 @@
 import './Nav.css';
 import { NavLink, Link } from 'react-router-dom';
+import UserContext from '../UserContext/UserContext';
+import { useContext } from 'react';
 
 const Nav = () => {
+  const {currentUser} = useContext(UserContext);
+
   return (
     <section className='banner-container'>
       <Link to='/' className='title' ><h1>SOUND STASH</h1></Link>
+      {currentUser && 
       <nav className='navigation-tabs'>
         <NavLink to="/collections" className='nav'>COLLECTIONS</NavLink>
         <NavLink to="/journal" className='nav'>JOURNAL</NavLink>
         <NavLink to="/discover" className='nav'>DISCOVER</NavLink>
-      </nav>
-      <p className='user-profile'>User</p>
+      </nav>}
+      {!currentUser && <button className='user-profile'>LOGIN</button>}
     </section>
   );
 };
