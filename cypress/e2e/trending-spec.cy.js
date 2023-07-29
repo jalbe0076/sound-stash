278572b3241da2ebac1dd0ab858b973f40649547
Jason Alberto <alberto.jason@gmail.com>
2023-07-29 00:36:51 -0400

    Add trending tests

--- cypress/e2e/trending-spec.cy.js
+++ cypress/e2e/trending-spec.cy.js
@@ -6,24 +6,43 @@ describe('should be able to view collections', () => {
        fixture: 'trending.json'
       }
     )
-
-    cy.visit('http://localhost:3000/trending')
+    cy.visit('http://localhost:3000/login')
   })
 
-  it('should have trending albums and tabs when a user is logged in', () => {
+  it('Should see the navigation bar and trending albums when a user is logged in along with a logout button', () => {
     cy.get('[value="user2"]').click()
       .get('form > .standard-btn').click()
+      .url().should('include', '/')
       .get('.banner-container').contains('h1', 'SOUND STASH')
       .get('.banner-container').contains('a', 'DISCOVER')
       .get('.recommendedContainer').find('.link').should('have.length', 5).first().find('img').should('have.attr', 'src')
       .get('.recommendedContainer').find('.link').first().find('.album').contains('p', 'Queens')
       .get('.recommendedContainer').find('.link').last().find('img').should('have.attr', 'src')
       .get('.recommendedContainer').find('.link').last().find('.album').contains('p', 'Akira')
-      .get('.banner-container').contains('a', 'COLLECTIONS').click()
-      .get('.album--grid').find('.results--card').should('have.length', 5).first().find('img').should('have.attr', 'src')
-      .get('.album--grid').find('.results--card').first().contains('p', 'In Times New')
-      .get('.results--card').next().contains('p', 'Star Wars')
-      .get('.album--grid').find('.results--card').last().find('img').should('have.attr', 'src')
-      .get('.album--grid').find('.results--card').last().contains('p', 'Delta Kream')
+      .get('.logout-btn').contains('button', 'LOGOUT')
   })
+
+  it('Should see the same information when another user is logged in', () => {
+    cy.get('[value="user1"]').click()
+      .get('form > .standard-btn').click()
+      .url().should('include', '/')
+      .get('.banner-container').contains('h1', 'SOUND STASH')
+      .get('.banner-container').contains('a', 'DISCOVER')
+      .get('.recommendedContainer').find('.link').should('have.length', 5).first().find('img').should('have.attr', 'src')
+      .get('.recommendedContainer').find('.link').first().find('.album').contains('p', 'Queens')
+      .get('.recommendedContainer').find('.link').last().find('img').should('have.attr', 'src')
+      .get('.recommendedContainer').find('.link').last().find('.album').contains('p', 'Akira')
+      .get('.logout-btn').contains('button', 'LOGOUT')
+  })
+
+  it('When no user is logged in we should not see the havigation bar or the logout button, a login button and trending albums should be visible', () => {
+    cy.get('[value="no-user"]').click()
+    .url().should('include', '/')
+    .get('.banner-container').contains('h1', 'SOUND STASH')
+    .get('[href="/collections"]').should('not.exist')
+    .get('[href="/journal"]').should('not.exist')
+    .get('[href="/discover"]').should('not.exist')
+    .get('.user-profile').contains('button', 'LOGIN')
+  })
+
 })
