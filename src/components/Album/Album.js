b065ea2d36b7a21bf8d7f0cbaad2ae2c35db7b63
Joshua Martin <ursa8minority@gmail.com>
2023-07-30 17:36:51 -0600

    CSS refactor on album specifics

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -63,27 +63,33 @@ function Album({handleApiError}) {
           <div className="buttons-container">
             <button className="add-to-collections-button" onClick={() => handleAddToCollections()}>Add to Collections</button>
             {!modal && <button className="journal-button" onClick={showModal}>Add to Journal Entry</button>}
-          </div>}
-        <img classname="cover-image" src={coverImg} alt={`Cover art for ${title}`} />
-        <h2>{title}</h2>
-        <p>Artist: {artist}</p>
-        <p>Release Date: {releaseDate}</p>
-        <p>Genre: {genre}</p>
-        {styles && styles.length > 0 && <p>Styles: {styles.join(', ')}</p>}
-        {tracklist && tracklist.length > 0 && (
-          <>
-            <h3>Tracklist:</h3>
-            <ol>
+          </div>
+        }
+    
+        <div className="album-tracklist-container">
+          {tracklist && tracklist.length > 0 && (
+            <div className="tracklist-container">
+                  <h3 className="tracklist-title">Tracklist:</h3>
               {tracklist.map((track, index) => (
-                <li key={index}>{track}</li>
+                <p key={index}>{track}</p>
               ))}
-            </ol>
-          </>
-        )}
+            </div>
+          )}
+    
+          <div className="album-details-container">
+            <img className="cover-image" src={coverImg} alt={`Cover art for ${title}`} />
+            <h2 className="album-title">{title}</h2>
+            <p>Artist: {artist}</p>
+            <p>Release Date: {releaseDate}</p>
+            <p>Genre: {genre}</p>
+            {styles && styles.length > 0 && <p>Styles: {styles.join(', ')}</p>}
+          </div>
+        </div>
+    
         {modal && <Form id={id} {...albumDetails} showModal={showModal} />}
       </div>
     );
   }
 }
-
+    
 export default Album;
