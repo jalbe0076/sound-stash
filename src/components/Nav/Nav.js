b1c06d6292ddc311927f7899d4009a5fb978a088
Jason Alberto <alberto.jason@gmail.com>
2023-08-10 15:15:46 -0400

    Conditionally hide the login button on login page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import './Nav.css';
-import { NavLink, Link, Navigate, useNavigate } from 'react-router-dom';
+import { NavLink, Link, useLocation, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import UserContext from '../UserContext/UserContext';
 import { useContext } from 'react';
@@ -7,13 +7,14 @@ import { useContext } from 'react';
 const Nav = () => {
   const {setCurrentUser, isUserLoggedIn, setIsUserLoggedIn} = useContext(UserContext);
   const navigate = useNavigate();
+  const location = useLocation()
 
   const logoutUser = () => {
     navigate('/');
     setCurrentUser(false);
-    setIsUserLoggedIn(false)
+    setIsUserLoggedIn(false);
   }
-
+console.log(location.pathname !== '/login')
   return (
     <section className='banner-container'>
       <Link to='/' className='title' ><h1>SOUND STASH</h1></Link>
@@ -23,9 +24,18 @@ const Nav = () => {
         <NavLink to="/journal" className='nav'>JOURNAL</NavLink>
         <NavLink to="/discover" className='nav'>DISCOVER</NavLink>
       </nav>}
-      {isUserLoggedIn ? 
-          <button className='logout-btn' onClick={() => logoutUser()}><img className='user-profile user-icon' src={process.env.PUBLIC_URL + '/images/user-icon.png'}/>LOGOUT</button> 
-        : <button className='user-profile standard-btn' onClick={() => navigate('/login')}>LOGIN</button>
+      { location.pathname !== '/login' ? (
+          isUserLoggedIn ? (
+            <button className='logout-btn' onClick={() => logoutUser()}>
+              <img className='user-profile user-icon' src={process.env.PUBLIC_URL + '/images/user-icon.png'} alt="User Icon" />
+              LOGOUT
+            </button>
+          ) : (
+            <button className='user-profile standard-btn' onClick={() => navigate('/login')}>
+              LOGIN
+            </button>
+          )
+        ) : null
       }
     </section>
   );
